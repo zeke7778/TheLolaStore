6938115c86460231c6dc809dcbefe9d23f5c59de
amankumarekka <amankumarekka96@gmail.com>
2025-11-18 12:01:24 +0530

    update API URL2

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,4 +1,4 @@
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
+const API_URL = process.env.REACT_APP_API_URL ;
 
 export const registerUser = async (data) => {
   const res = await fetch(`${API_URL}/register`, {
